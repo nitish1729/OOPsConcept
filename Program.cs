01bd7fb36e50a6b355b67675026c9d54b47499f4
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-07 02:43:35 +0530

    added .gitignore files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public class OOPConcept
+{
+    public static void Welcome()
+    {
+        Console.WriteLine("Welcome to the OOPs Concept");
+    }
+
+    public static void OOPTypes()
+    {
+        Console.WriteLine("There are 4 basic concept of OOPs");
+    }
+    public void typee()
+    {
+        string typee = "Abstraction,Encapsulation, Inheritance, Polymorphis,";
+        Console.WriteLine(typee);
+    }
+    public static void Main()
+    {
+        OOPConcept oopc = new OOPConcept();
+        Welcome();
+        OOPTypes();
+        oopc.typee();
+        Console.ReadLine();
+    }
+}
+
