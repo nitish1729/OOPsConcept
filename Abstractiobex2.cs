dc4daa46cd05393ca606bd0125c3671975f031ca
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-07 02:51:57 +0530

    Absraction practice

---
+++ Abstractiobex2.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPConcept
+//{
+//    public class Laptop
+//    {
+//        private string brand;
+//       private string model;
+//       public string Brand
+//       {
+//          get { return brand; }
+//          set { brand = value; }
+//       }
+//       public string Model
+//       {
+//          get { return model; }
+//          set { model = value; }
+//       }
+//       public void LaptopDetails()
+//       {
+//          Console.WriteLine("Brand: " + Brand);
+//          Console.WriteLine("Model: " + Model);
+//       }
+//       public void LaptopKeyboard()
+//       {
+//          Console.WriteLine("Type using Keyword");
+//       }
+//       private void MotherBoardInfo()
+//       {
+//          Console.WriteLine("MotheBoard Information");
+//       }
+//       private void InternalProcessor()
+//       {
+//          Console.WriteLine("Processor Information");
+//       }
+//    }
+//    class Laptop1
+//    {
+//   public static void Main(string[] args)
+//        {
+//          Laptop l = new Laptop();
+//          l.Brand = "Dell";
+//          l.Model = "Inspiron 14R";
+//          l.LaptopDetails();
+//          Console.WriteLine("\nPress Enter Key to Exit..");
+//          Console.ReadLine();
+//       }
+//    }
+//}
---
+++ AbstractionC.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPConcept
+//{
+//   public class Employee
+//    {
+//        public string CompanyName;
+//        public int EmpID;
+//        public string EmpName;
+//        public double GrossPay;
+//        double TaxDeducation = 0.05;
+//        double netSalary;
+
+//        public Employee(string ECompanyname, string EName, int EID, double EGrosspay)
+//        {
+//            this.CompanyName = ECompanyname;
+//            this.EmpID = EID;
+//            this.EmpName = EName;
+//            this.GrossPay = EGrosspay;
+                
+//        }
+//        void CalculateSalary()
+//        {
+//            if (GrossPay >= 22000)
+//            {
+//                netSalary = GrossPay - (TaxDeducation*GrossPay);
+//                Console.WriteLine("Your salary is  " + netSalary);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Your salary is : "+GrossPay);
+//            }
+//        }
+//        public void ShowEmployeeDetails()
+//        {
+//            this.CalculateSalary();
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee Nitish = new Employee("Axa business service","Nitish Mehta",
+//                280880, 25000);
+//            Nitish.ShowEmployeeDetails();
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
